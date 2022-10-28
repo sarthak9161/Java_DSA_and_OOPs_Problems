18f73bb01eb04b00748ef56265f3bf78edff20c2
Sarthak M. Wagh <sarthakwagh16@gmail.com>
2022-10-28 11:56:46 +0530

    this and super keyword code

--- src/sarthak/this_super_keyword.java
+++ src/sarthak/this_super_keyword.java
@@ -1,7 +1,42 @@
 package sarthak;
 
+// this : when we have to refer the object of the class which is being created then we use this keyword
+class ekclass{
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    ekclass(int v)
+    {
+        this.a=v;
+    }
+}
+
+// super : it is used to refer the immediate parent class object
+// used to invoke any methods of the parent class
+// used to invoke any constructor of the parent class
+// by default it will invoke default constru bt if custom constru is need to invoke then use super keyword
+
+class doclass extends ekclass{
+    doclass(int c)
+    {
+        super(c);   // this will call the constructor of the parent class
+        // i.e muze aise constructor ko called krna hai parent class me se jo int value leta hai
+        System.out.println("me ek constructor hu !");
+    }
+
+}
 public class this_super_keyword {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        ekclass e=new ekclass(65);
+        System.out.println("the value is:"+e.getA());
+
+        doclass d=new doclass(200);
+
+
+
     }
 }
